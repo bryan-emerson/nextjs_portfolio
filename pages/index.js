01b3b28307ca220409ce5900f52fa1ee50c5a2ec
Bryan Emerson <bryan.l.emerson@gmail.com>
2023-06-12 18:01:24 -0700

    nav rendered

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+import Navbar from '../components/Navbar.js';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <div>hello brodie</div>
+    <Navbar />
     </>
   )
 }
