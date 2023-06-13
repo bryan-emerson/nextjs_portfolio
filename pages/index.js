df87d3692ed5a3ca5ab996b173c4af3795b34208
Bryan Emerson <bryan.l.emerson@gmail.com>
2023-06-13 09:29:35 -0700

    large bg image rendered

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Paper } from '@mui/material';
+
+import large_background from '../public/assets/large_background.jpg'
+import largeSubject from '../public/assets/largeSubject.png'
+import mobile_background from '../public/assets/mobile_background.jpg'
 
 import Navbar from '../components/Navbar.js';
 
@@ -10,7 +13,17 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <Navbar />
+      <Paper>
+        <Navbar />
+        <div className={styles.back} style={{
+          backgroundImage: `url('${large_background.src}')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          zIndex: 0
+        }} />
+      </Paper>
     </>
   )
 }
+
+// Photo assets just added for index.js, next i need to implement parralax
